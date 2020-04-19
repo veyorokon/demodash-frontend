f199afcc2ca42a4386d527880e6c54349efdcaba
Vahid <veyorokon@gmail.com>
2020-04-19 19:35:26 -0400

    Fixed extra padding in howitworks.

--- react/src/views/Landing/Sections/HowItWorks.js
+++ react/src/views/Landing/Sections/HowItWorks.js
@@ -70,7 +70,6 @@ class HowItWorks extends React.Component {
           <Flex
             flexDirection="column"
             bg="whites.0"
-            p={3}
             pl={r("2 ----> 3 4 5 6 -> 8")}
             pr={r("2 ----> 3 4 5 6 -> 8")}
             m={0}
