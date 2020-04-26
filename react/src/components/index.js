2868c59e42a7cb4fa61a769d9548865f34abcfc3
Vahid <veyorokon@gmail.com>
2020-04-25 21:30:14 -0400

    Added styling.

--- react/src/components/index.js
+++ react/src/components/index.js
@@ -1,17 +1,7 @@
 import {Flex, Image, Box, Button, Input, Text, Video, Grid, Link} from "./core";
-
+import FeatureLink from "./composite/FeatureLink";
 import Animate, {appear} from "./core/animation";
 import Hidden from "./core/hidden";
-import {
-  Tabs,
-  TabHeaders,
-  TabContents,
-  TabComponent,
-  Tab,
-  Panel
-} from "./composite/tabs";
-import LeftCallOut from "./composite/leftCallout";
-
 import {ImageBox, Section} from "./styled";
 export {
   Text,
@@ -23,16 +13,10 @@ export {
   Button,
   Input,
   Video,
-  Tabs,
-  TabHeaders,
-  TabContents,
-  Tab,
-  Panel,
   ImageBox,
   Section,
   Grid,
-  TabComponent,
-  LeftCallOut,
   Link,
+  FeatureLink,
   appear
 };
