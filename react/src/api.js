e58145db704493b101a02d9f7ceb83d8e0da4e75
Vahid <veyorokon@gmail.com>
2020-11-22 14:12:48 -0500

    Added env vars

--- react/src/api.js
+++ react/src/api.js
@@ -1,6 +1,6 @@
-export const API_SERVER =
-  process.env.REACT_APP_API_SERVER || "http://192.168.200.238:8000";
+export const API_HOST =
+  process.env.REACT_APP_API_HOST || "http://192.168.200.238:8000";
 
-export const API_MEDIA = `${API_SERVER}/media/`;
+export const API_MEDIA = `${API_HOST}/media/`;
 
 export const DASHBOARD_HOST = process.env.REACT_APP_DASHBOARD_HOST || "";
